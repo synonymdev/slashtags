3aba1f3144936e4ba3e4ecacf1b4eabc3ebf1446
nazeh <ar.nazeh@gmail.com>
2022-04-27 10:26:38 +0300

    chore(setup): enable testing with the mainnet

--- packages/sdk/test/helpers/setup-sdk.js
+++ packages/sdk/test/helpers/setup-sdk.js
@@ -1,7 +1,7 @@
 import { SDK } from '../../src/sdk.js'
 
-const { RELAY_URL, BOOTSTRAP } = process.env
-const bootstrap = JSON.parse(BOOTSTRAP)
+const { RELAY_URL, BOOTSTRAP, MAINNET } = process.env
+const bootstrap = MAINNET ? undefined : JSON.parse(BOOTSTRAP)
 
 export function sdk (opts) {
   return SDK.init({
