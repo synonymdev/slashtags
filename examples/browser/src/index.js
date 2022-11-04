320154be230382e3d59c694ad0491e72c34c9c11
Mike O'Bank <mike.obank@pm.me>
2022-11-04 14:47:22 +0100

    Commit new file

---
+++ examples/browser/src/index.js
@@ -0,0 +1,35 @@
+import SDK from '@synonymdev/slashtags-sdk'
+import b4a from 'b4a'
+
+(async () => {
+
+  let key
+
+  {
+    const sdk = new SDK({ relay: 'ws://localhost:45475' })
+    const alice = sdk.slashtag()
+    console.log({ alice: alice.url })
+    const drive = alice.drivestore.get()
+    await drive.put('/profile.json', b4a.from(JSON.stringify({ name: 'Alice' })))
+
+    console.log("Announcing Alice's public drive...")
+    await sdk.join(drive.discoveryKey)?.flushed()
+
+    key = alice.key
+  }
+
+  {
+    const sdk = new SDK({ relay: 'ws://localhost:45475' })
+    const bob = sdk.slashtag()
+    console.log({ bob: bob.url })
+    const drive = sdk.drive(key)
+    console.log("Resolving Alice's public drive...")
+    try {
+      const buf = await drive.get('/profile.json')
+      const profile = JSON.parse(b4a.toString(buf))
+      console.log("Profile:", profile)
+    } catch (err) {
+      console.error("failed getting, parsing profile")
+    }
+  }
+})()
